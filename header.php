a279858e0ba1ed3e922c4d2a547c338c569700c7
0778314775 <46378582+0778314775@users.noreply.github.com>
2023-02-11 00:41:38 +0000

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=divice-width, initial-scale=1.0 minimum-scale=1.0 maximum-scale=1.0 user-scalable=no">
-<meta property="og:title" content="Her Voice Liberia">
-<meta property="og:url" content="www.hervoiceliberia.org">
+<meta property="og:title" content="">
+<meta property="og:url" content="">
 <meta property="og:url" content="img/logo.png">
 <meta property="og:type" content="website">
-<meta property="og:description" content="Her Voice Liberia is a non- Governmental (NG0), Non-profit organization established in March 2017. This organization was established in response to maltreatment of women and children, and violation of their rights and other practices, which have been alarmingly wide spread and prevalent since the Liberian Civil War.">
-<meta http-equiv="author" content="Her Voice Liberia">
+<meta property="og:description" content="">
+<meta http-equiv="author" content="">
 <link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
